d42de21158efa2d8f9b239c4d0ca78fdcdc37075
signalfern <signalfern.dev@gmail.com>
2026-06-05 13:19:40 +0530

    Score
    Added score and world borders

--- flappy-bird/scripts/level.gd
+++ flappy-bird/scripts/level.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
 @onready var  pipe_scene = preload("res://scenes/pipe_pair.tscn")
-
-
+@onready var score_label = $CanvasLayer/Score_label
+var score:= 0 
 
 func _on_timer_timeout():
 	var pipe = pipe_scene.instantiate()
+	pipe.Score_changed.connect(on_score_changed)
 	pipe.position.x = 1154
 	add_child(pipe)
 	
 
 func _ready() -> void:
 	print("timer started")
-	
+
+
+func on_score_changed(new_score):
+	score = score + new_score
+	score_label.text = "Score :" + str(score)
+	print(score)
--- flappy-bird/scripts/pipe_pair.gd
+++ flappy-bird/scripts/pipe_pair.gd
@@ -2,14 +2,20 @@ extends Node2D
 
 @onready var Y = $".".position.y
 @export var Speed := 200.0
+signal Score_changed(new_score : int) 
 
 
 func gap_postion():
-	var num := randf_range(-140.0,320.0)
+	var num := randf_range(-140.0,250.0)
 	position.y = Y + num
 
 func _ready() -> void:
 	gap_postion()
+	
 
 func _process(delta: float) -> void:
 	position.x -= Speed * delta 
+
+
+func _on_score_area_body_entered(body: Node2D) -> void:
+	Score_changed.emit(1)
